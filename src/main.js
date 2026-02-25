c65dc45f897458ab352b58e19c7cf42d80d7d767
Petrakova Yulia <julia.efr7@gmail.com>
2026-02-25 16:02:13 +0300

    Шапка+пагинация

--- src/main.js
+++ src/main.js
@@ -8,7 +8,8 @@ import {processFormData} from "./lib/utils.js";
 
 import {initTable} from "./components/table.js";
 // @todo: подключение
-
+import {initPagination} from "./components/pagination.js";
+import {initSorting} from "./components/sorting.js";
 
 // Исходные данные используемые в render()
 const {data, ...indexes} = initData(sourceData);
@@ -19,11 +20,14 @@ const {data, ...indexes} = initData(sourceData);
  */
 function collectState() {
     const state = processFormData(new FormData(sampleTable.container));
+    const rowsPerPage = parseInt(state.rowsPerPage);    // приведём количество страниц к числу
+const page = parseInt(state.page ?? 1);                // номер страницы по умолчанию 1 и тоже число
 
-    return {
-        ...state
-    };
-}
+return {                                            // расширьте существующий return вот так
+    ...state,
+    rowsPerPage,
+    page
+}};
 
 /**
  * Перерисовка состояния таблицы при любых изменениях
@@ -33,20 +37,34 @@ function render(action) {
     let state = collectState(); // состояние полей из таблицы
     let result = [...data]; // копируем для последующего изменения
     // @todo: использование
-
-
+    result = applyPagination(result, state, action);
+    result = applySorting(result, state, action);
     sampleTable.render(result)
 }
 
 const sampleTable = initTable({
     tableTemplate: 'table',
     rowTemplate: 'row',
-    before: [],
-    after: []
+    before: ['header'],
+    after: ['pagination']
 }, render);
 
 // @todo: инициализация
-
+const applyPagination = initPagination(
+    sampleTable.pagination.elements,             // передаём сюда элементы пагинации, найденные в шаблоне
+    (el, page, isCurrent) => {                    // и колбэк, чтобы заполнять кнопки страниц данными
+        const input = el.querySelector('input');
+        const label = el.querySelector('span');
+        input.value = page;
+        input.checked = isCurrent;
+        label.textContent = page;
+        return el;
+    }
+);
+const applySorting = initSorting([        // Нам нужно передать сюда массив элементов, которые вызывают сортировку, чтобы изменять их визуальное представление
+    sampleTable.header.elements.sortByDate,
+    sampleTable.header.elements.sortByTotal
+]);
 
 const appRoot = document.querySelector('#app');
 appRoot.appendChild(sampleTable.container);
